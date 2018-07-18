169881b38279e2dcc6dd1785c66697c1ba4bb326
estods3 <estods76@gmail.com>
2018-07-17 23:20:36 -0400

    environment class set to initialize window with start, end and obsticles

--- environment.py
+++ environment.py
@@ -18,16 +18,35 @@ class environment:
 		y = random.randint(0, int(screenSizeY - 1))
 		pygame.draw.circle(self.screen, color, (x,y), 15, 0)		
 		return x, y
-	def startAndEnd(self):
+	def startAndEndPoints(self):
 		color = (0, 255, 0)
-		x = random.randint(0, int((1/3.0) * screenSizeX))
-		y = random.randint(0, int(screenSizeY))
-
+		x1 = random.randint(0, int((1/3.0) * screenSizeX))
+		y1 = random.randint(0, int(screenSizeY))
+		pygame.draw.circle(self.screen, color, (x1,y1), 15, 0)
+		x2 = random.randint(int((2/3.0) * screenSizeX), screenSizeX)
+		y2 = random.randint(0, int(screenSizeY))
+		pygame.draw.circle(self.screen, color, (x2,y2), 15, 0)
+		return x1, y1, x2, y2
+	
 e = environment()
+e.startAndEndPoints()
 e.addObsticle()
 e.addObsticle()
-for event in pygame.event.get():
-	if event.type == pygame.QUIT:
-		pygame.quit()
-		sys.exit()
+while(True):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+	#todo clean the screen
+
+
+
+	#calculate the best sample in generation
+
+
+	#populate best samples for next generation
+
+
+	#update the screen
 	pygame.display.update()
